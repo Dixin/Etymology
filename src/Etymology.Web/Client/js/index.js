f1b489547e1372ed60e6a13997fd9a99e9e60a90
Dixin Yan <dixinyan@live.com>
2017-09-24 17:08:07 -0700

    Use ECMAScript syntax.

--- src/Etymology.Web/Client/js/index.js
+++ src/Etymology.Web/Client/js/index.js
@@ -1,17 +1,17 @@
-﻿require("ie10-viewport-bug-workaround.js");
-var Spin = require("spin.js");
-var Ladda = require("ladda");
-var Cookies = require("js-cookie");
+﻿import "ie10-viewport-bug-workaround.js";
+import Spin from "spin.js";
+import Ladda from "ladda";
+import Cookies from "js-cookie";
 
-var global = window;
-var $global = $(global);
-var location = global.location;
-var $document = $(global.document);
+const global = window;
+const $global = $(global);
+const location = global.location;
+const $document = $(global.document);
 
-var navOffset = 0;
+let navOffset = 0;
 
-var animateToHash = function (hash) {
-    var $hash = $(hash.indexOf("#") === 0 ? hash : "#" + hash); // IE does not support starsWith.
+const animateToHash = hash => {
+    const $hash = $(hash.indexOf("#") === 0 ? hash : `#${hash}`); // IE does not support starsWith.
     if ($hash.length > 0) {
         $('html, body').animate({
             scrollTop: $hash.offset().top - navOffset
@@ -19,11 +19,11 @@ var animateToHash = function (hash) {
     }
 };
 
-var setHash = function (hash) {
+const setHash = hash => {
     if (hash.indexOf("#") === 0) { // IE does not support starsWith.
         hash = hash.substring(1);
     }
-    var $hash = $("#" + hash);
+    const $hash = $(`#${hash}`);
     if ($hash.length > 0) {
         $hash.removeAttr("id");
         location.hash = hash;
@@ -33,95 +33,91 @@ var setHash = function (hash) {
     }
 };
 
-var commonChars = "安八白百败般邦保宝北贝比毕辟宾兵丙秉伯帛卜不步采仓曹长厂车彻臣辰晨成乘承迟齿赤冲丑初出楚传春此次大丹单旦稻得登邓帝典奠丁鼎东冬动斗豆断队对多而儿耳二伐反方分封逢夫服福弗甫斧复父妇干刚高羔告戈格庚工攻公弓鼓古谷官观光归龟鬼癸国果亥好禾何河侯厚后呼乎壶虎化淮黄惠会获或基箕吉及疾即己季祭既家甲监见姜降角教解巾斤今进兢京井竞九酒旧沮句爵君考可克客口寇来牢老乐丽利立良林陵令柳六聋卢鲁麓鹿陆旅率洛马买麦卯枚眉每媚门蒙盟皿敏明鸣名命莫牡母木目牧乃南男内逆年鸟宁牛农奴女诺旁彭品仆圃戚七齐祈启气千前遣羌强且侵秦卿丘区取去犬壬人日戎如入若三散山上少绍射涉申生省师尸十石时食史矢使始士事氏室视首受叔蜀黍束戍水司丝死四巳宋宿绥岁孙它唐天田同徒土退屯外王亡韦唯未畏位卫文闻问我吴武五午舞戊勿昔析夕袭喜下先相祥向象小效辛新兴行兄休羞戌徐畜宣旋学旬亚言炎衍央扬羊阳页一依伊衣夷疑宜彝乙易邑肄义因寅尹永用幽酉有友又幼于盂虞余鱼渔雨羽玉御育元员曰月允宰载再在葬增曾乍宅召折贞征正郑之执止旨至中终仲舟周州帚朱逐贮祝追兹子自宗足卒族祖尊作";
-var commonCharsLength = commonChars.length;
+const commonChars = "安八白百败般邦保宝北贝比毕辟宾兵丙秉伯帛卜不步采仓曹长厂车彻臣辰晨成乘承迟齿赤冲丑初出楚传春此次大丹单旦稻得登邓帝典奠丁鼎东冬动斗豆断队对多而儿耳二伐反方分封逢夫服福弗甫斧复父妇干刚高羔告戈格庚工攻公弓鼓古谷官观光归龟鬼癸国果亥好禾何河侯厚后呼乎壶虎化淮黄惠会获或基箕吉及疾即己季祭既家甲监见姜降角教解巾斤今进兢京井竞九酒旧沮句爵君考可克客口寇来牢老乐丽利立良林陵令柳六聋卢鲁麓鹿陆旅率洛马买麦卯枚眉每媚门蒙盟皿敏明鸣名命莫牡母木目牧乃南男内逆年鸟宁牛农奴女诺旁彭品仆圃戚七齐祈启气千前遣羌强且侵秦卿丘区取去犬壬人日戎如入若三散山上少绍射涉申生省师尸十石时食史矢使始士事氏室视首受叔蜀黍束戍水司丝死四巳宋宿绥岁孙它唐天田同徒土退屯外王亡韦唯未畏位卫文闻问我吴武五午舞戊勿昔析夕袭喜下先相祥向象小效辛新兴行兄休羞戌徐畜宣旋学旬亚言炎衍央扬羊阳页一依伊衣夷疑宜彝乙易邑肄义因寅尹永用幽酉有友又幼于盂虞余鱼渔雨羽玉御育元员曰月允宰载再在葬增曾乍宅召折贞征正郑之执止旨至中终仲舟周州帚朱逐贮祝追兹子自宗足卒族祖尊作";
+const commonCharsLength = commonChars.length;
 
-$document.on("ready", function () {
-    navOffset = $("nav").height();
+$document.on("ready", () => {
+    navOffset = $("nav").height() - 1;
 
-    $("a.etymology-link").on('click', function () {
-        animateToHash(this.hash);
+    $("a.etymology-link").on('click', event => {
+        animateToHash(event.currentTarget.hash);
     });
 
-    var buttons = $("#etymologySearchButton, #etymologyNavSearchButton, #etymologyRandomButton").map(function () {
-        return Ladda.create(this);
-    });
-    var $error = $("#etymologyError");
-    var $result = $("#etymologyResult");
-    var search = function (chinese) {
+    const buttons = $("#etymologySearchButton, #etymologyNavSearchButton, #etymologyRandomButton").map((index, element) => Ladda.create(element));
+    const $error = $("#etymologyError");
+    const $result = $("#etymologyResult");
+    const search = chinese => {
         $("#etymologySearchChar, #etymologyNavSearchChar").val(chinese);
 
-        $.each(buttons, function (index, button) {
+        $.each(buttons, (index, button) => {
             button.start();
         });
-        var token = Cookies.get("Bronze");
+        const token = Cookies.get("Bronze");
         $.ajax({
             type: "POST",
-            data: { chinese: chinese, Bronze: token },
+            data: { chinese, Bronze: token },
             dataType: "html",
             url: "etymology",
             headers: {
                 Seal: token
             }
-        }).done(function (data) {
+        }).done(data => {
             $error.hide();
             $result.html(data).show();
 
             savePositions();
             animateToHash(chinese, true);
-        }).fail(function (jqXHR, textStatus, error) {
+        }).fail((jqXHR, textStatus, error) => {
             $result.hide();
-            var message = !jqXHR.status || !jqXHR.responseText ? "Network connection failed." : "[" + jqXHR.status + " " + textStatus + "] " + error + ". " + jqXHR.responseText;
+            const message = !jqXHR.status || !jqXHR.responseText ? "Network connection failed." : `[${jqXHR.status} ${textStatus}] ${error}. ${jqXHR.responseText}`;
             $error.find("#etymologyErrorMessage").text(message).end().show();
 
             savePositions();
             animateToHash("#etymologyError");
-        }).always(function () {
-            $.each(buttons, function (index, button) {
+        }).always(() => {
+            $.each(buttons, (index, button) => {
                 button.stop();
             });
         });
     };
 
-    $("#etymologySearch, #etymologyNavSearch").on("submit", function (event) {
+    $("#etymologySearch, #etymologyNavSearch").on("submit", event => {
         event.preventDefault();
-        var chinese = "";
-        if (this.id === "etymologySearch") {
+        let chinese = "";
+        if (event.currentTarget.id === "etymologySearch") {
             chinese = $("#etymologySearchChar").val();
         }
-        if (this.id === "etymologyNavSearch") {
+        if (event.currentTarget.id === "etymologyNavSearch") {
             chinese = $("#etymologyNavSearchChar").val();
         }
-        if ($("#" + chinese).length > 0) {
+        if ($(`#${chinese}`).length > 0) {
             animateToHash(chinese);
         } else {
             location.hash = chinese;
         }
     });
 
-    $("#etymologyRandomButton").on("click", function () {
-        var index = Math.floor(Math.random() * commonCharsLength + 1);
+    $("#etymologyRandomButton").on("click", () => {
+        const index = Math.floor(Math.random() * commonCharsLength + 1);
         location.hash = commonChars[index];
     });
 
-    var isChinese = function (value) {
-        return /[\u3400-\u9FBF]/.test(value);
-    };
-    var searchFromHash = function () {
-        var chineseFromHash = location.hash.substring(1);
-        if (isChinese(chineseFromHash) && $("#" + chineseFromHash).length === 0) {
+    const isChinese = value => /[\u3400-\u9FBF]/.test(value);
+    const searchFromHash = () => {
+        const chineseFromHash = location.hash.substring(1);
+        if (isChinese(chineseFromHash) && $(`#${chineseFromHash}`).length === 0) {
             search(chineseFromHash);
         } else {
-            var decoded = global.decodeURIComponent(chineseFromHash);
-            if (isChinese(decoded) && $("#" + decoded).length === 0) {
+            const decoded = global.decodeURIComponent(chineseFromHash);
+            if (isChinese(decoded) && $(`#${decoded}`).length === 0) {
                 search(decoded);
             }
         }
-    }
+    };
 
-    var oldHash = location.hash.substring(1);
-    $global.on('hashchange', function () {
-        var newHash = location.hash.substring(1);
+    let oldHash = location.hash.substring(1);
+    $global.on('hashchange', () => {
+        const newHash = location.hash.substring(1);
         if (oldHash !== newHash) {
             searchFromHash();
             oldHash = newHash;
@@ -132,18 +128,18 @@ $document.on("ready", function () {
     searchFromHash();
 });
 
-$global.on("load", function () {
-    $("#videos .media-left a").each(function () {
-        var $link = $(this);
-        $link.css({ "background-image": 'url(' + $link.data("background") + ')' });
+$global.on("load", () => {
+    $("#videos .media-left a").each((index, element) => {
+        const $link = $(element);
+        $link.css({ "background-image": `url(${$link.data("background")})` });
     });
 });
 
-var positions = null;
-var savePositions = function () {
-    positions = $.map($(".etymology-nav"), function (element) {
-        var $element = $(element);
-        var position = $element.position();
+let positions = null;
+var savePositions = () => {
+    positions = $.map($(".etymology-nav"), element => {
+        const $element = $(element);
+        const position = $element.position();
         return {
             top: position.top - 100,
             bottom: position.top - 100 + $element.height(),
@@ -152,18 +148,18 @@ var savePositions = function () {
     });
 };
 
-var top = null;
-var currentHash = null;
-var scrollTimeoutId = null;
-$global.on("scroll", function () {
+let top = null;
+let currentHash = null;
+let scrollTimeoutId = null;
+$global.on("scroll", () => {
     if (scrollTimeoutId) {
         global.clearTimeout(scrollTimeoutId);
     }
-    scrollTimeoutId = global.setTimeout(function () {
-        var newTop = $document.scrollTop();
+    scrollTimeoutId = global.setTimeout(() => {
+        const newTop = $document.scrollTop();
         if (newTop !== top) {
             top = newTop;
-            $.each(positions, function (index, position) {
+            $.each(positions, (index, position) => {
                 if (position.top >= top || position.bottom <= top) {
                     return true;
                 }
@@ -178,5 +174,5 @@ $global.on("scroll", function () {
     }, 300);
 });
 
-require("./baidu");
+import "./baidu";
 require("./google");
