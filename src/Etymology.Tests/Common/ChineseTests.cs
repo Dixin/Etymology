9af2c6a7139ce7cde8b39bff2af59f76db127628
Dixin Yan <dixinyan@live.com>
2018-05-06 01:26:19 -0700

    Update tests.

--- src/Etymology.Tests/Common/ChineseTests.cs
+++ src/Etymology.Tests/Common/ChineseTests.cs
@@ -31,7 +31,8 @@
             ("31F0", "ㇰ"),
             ("F900", "豈"),
             ("FE3D", "︽"),
-            ("FF6C", "ｬ")
+            ("FF6C", "ｬ"),
+            ("2B8B8", "𫢸")
         };
 
         internal static readonly List<string> OtherCharacters = new List<string>()
@@ -55,26 +56,7 @@
         [TestMethod]
         public void TextToCodePointTest()
         {
-            Assert.AreEqual("4E00", "一".TextToCodePoint());
-            Assert.AreEqual("3400", "㐀".TextToCodePoint());
-            Assert.AreEqual("3426", "㐦".TextToCodePoint());
-            Assert.AreEqual("20000", "𠀀".TextToCodePoint());
-            Assert.AreEqual("2A700", "𪜀".TextToCodePoint());
-            Assert.AreEqual("2B740", "𫝀".TextToCodePoint());
-            Assert.AreEqual("2F800", "丽".TextToCodePoint());
-            Assert.AreEqual("2E80", "⺀".TextToCodePoint());
-            Assert.AreEqual("2F00", "⼀".TextToCodePoint());
-            Assert.AreEqual("2FF0", "⿰".TextToCodePoint());
-            Assert.AreEqual("3025", "〥".TextToCodePoint());
-            Assert.AreEqual("3044", "い".TextToCodePoint());
-            Assert.AreEqual("30A2", "ア".TextToCodePoint());
-            Assert.AreEqual("3106", "ㄆ".TextToCodePoint());
-            Assert.AreEqual("31A1", "ㆡ".TextToCodePoint());
-            Assert.AreEqual("31CF", "㇏".TextToCodePoint());
-            Assert.AreEqual("31F0", "ㇰ".TextToCodePoint());
-            Assert.AreEqual("F900", "豈".TextToCodePoint());
-            Assert.AreEqual("FE3D", "︽".TextToCodePoint());
-            Assert.AreEqual("FF6C", "ｬ".TextToCodePoint());
+            ChineseCharacters.ForEach(item => Assert.AreEqual(item.HexCodePoint, item.Text.TextToCodePoint()));
         }
 
         [TestMethod]
